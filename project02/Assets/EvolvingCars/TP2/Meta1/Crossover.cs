2a851675f96bb17e7c0757254666ae36b90c2d74
tanhinho <miguel1leopoldo@gmail.com>
2024-04-19 22:18:19 +0100

    Implement crossover

--- project02/Assets/EvolvingCars/TP2/Meta1/Crossover.cs
+++ project02/Assets/EvolvingCars/TP2/Meta1/Crossover.cs
@@ -10,10 +10,6 @@ namespace GeneticSharp.Runner.UnityApp.Commons
 {
     public class Crossover : ICrossover
     {
-  
-
-        
-
         public int ParentsNumber { get; private set; }
 
         public int ChildrenNumber { get; private set; }
@@ -23,6 +19,11 @@ namespace GeneticSharp.Runner.UnityApp.Commons
         public bool IsOrdered { get; private set; } // indicating whether the operator is ordered (if can keep the chromosome order).
 
         protected float crossoverProbability;
+        
+        // Crossover options
+        public bool IsUniformCrossover {  get; private set; }
+        public int KPoints {  get; private set; }
+
 
 
         public Crossover(float crossoverProbability) : this(2, 2, 2, true)
@@ -40,15 +41,64 @@ namespace GeneticSharp.Runner.UnityApp.Commons
 
         public IList<IChromosome> Cross(IList<IChromosome> parents)
         {
+            /*
+            IList<IChromosome> cross;
+            if (IsUniformCrossover)
+                cross = UniformCrossover(parents);
+            else
+                cross = KPointCrossover(parents);
+            */
+
+            return UniformCrossover(parents);
+        }
+
+        public IList<IChromosome> UniformCrossover(IList<IChromosome> parents)
+        {
             IChromosome parent1 = parents[0];
             IChromosome parent2 = parents[1];
             IChromosome offspring1 = parent1.Clone();
             IChromosome offspring2 = parent2.Clone();
+
+            for (int i = 0; i < parent1.Length; i++)
+            {
+                if (RandomizationProvider.Current.GetInt(0, 2) == 1)
+                { 
+                    offspring1.ReplaceGene(i, parent2.GetGene(i)); 
+                    offspring2.ReplaceGene(i, parent1.GetGene(i));
+                }
+            }
+            return new List<IChromosome> { offspring1, offspring2 };
+        }
+
+        public IList<IChromosome> KPointCrossover(IList<IChromosome> parents)
+        {
+            IChromosome parent1 = parents[0];
+            IChromosome parent2 = parents[1]; 
+            IChromosome offspring1 = parent1.Clone();
+            IChromosome offspring2 = parent2.Clone();
+            int nrPoints = 0;    
+
+
+
+
+
+
+            for (int i = 1; i < parent1.Length-1; i++)
+            {
+                if (UnityEngine.Random.Range(0, 2) == 0)
+                    continue;
+
+                nrPoints++;
+                 
+                offspring1.ReplaceGenes(i, parent2.GetGenes().ToArray());                
+            }
+
+
+            
             offspring1.ReplaceGenes(0, parent2.GetGenes().ToArray());
             offspring2.ReplaceGenes(0, parent1.GetGenes().ToArray());
 
             return new List<IChromosome> { offspring1, offspring2 };
-            
         }
     }
 }
--- project02/Assets/EvolvingCars/TP2/Meta1/ParentSelection.cs
+++ project02/Assets/EvolvingCars/TP2/Meta1/ParentSelection.cs
@@ -16,14 +16,13 @@ public class ParentSelection : SelectionBase
     {
     }
 
-
-
-
     protected override IList<IChromosome> PerformSelectChromosomes(int number, Generation generation)
     {
 
         IList<CarChromosome> population = generation.Chromosomes.Cast<CarChromosome>().ToList();
         IList<IChromosome> parents = new List<IChromosome>();
+        
+            
 
         int[] randomIndexes = RandomizationProvider.Current.GetUniqueInts(number, 0, population.Count);
         while (parents.Count < number)
@@ -33,4 +32,33 @@ public class ParentSelection : SelectionBase
 
         return parents;
     }
+
+    private IList<IChromosome> RouletteSelection(int number, Generation generation)
+    {
+        IList<CarChromosome> population = generation.Chromosomes.Cast<CarChromosome>().ToList();
+        IList<IChromosome> parents = new List<IChromosome>();
+        float totalFitness = population.Sum(individual => individual.Fitness); 
+        List<(float, CarChromosome)> roulette = new List<(float, CarChromosome)> { };
+        foreach (var indivual in population)
+        {
+            roulette.Add((indivual.Fitness / totalFitness, indivual));
+        }
+
+
+        while(parents.Count < number)
+        {
+            float pick = RandomizationProvider.Current.GetFloat(0, 1);
+
+        }
+
+
+        int[] randomIndexes = RandomizationProvider.Current.GetUniqueInts(number, 0, population.Count);
+        while (parents.Count < number)
+        {
+            parents.Add(population[randomIndexes[parents.Count]]);
+        }
+
+        return parents;
+    }
+
 }
